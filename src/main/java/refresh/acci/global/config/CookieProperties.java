b1c0d8aa04c877814bf20ed0dd78f3a33111319a
Jungseokhwan <jsh2910@naver.com>
2026-02-05 23:43:45 +0900

    🔨refactor: Cookie sameSite 설정

---
+++ src/main/java/refresh/acci/global/config/CookieProperties.java
@@ -0,0 +1,17 @@
+package refresh.acci.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app.cookie")
+public class CookieProperties {
+    private boolean secure = true;
+    private String domain;
+    private String accessTokenSameSite = "Lax";
+    private String refreshTokenSameSite = "Strict";
+}

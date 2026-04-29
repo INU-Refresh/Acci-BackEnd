a6fb881f96265bb7fb5230a4a303ee577d36ecc1
d6nggyun <d6nggyun@gmail.com>
2026-04-30 00:27:38 +0900

    docs: AccidentTypeResponse 스웨거 스키마 추가

--- src/main/java/refresh/acci/domain/analysis/adapter/in/web/dto/res/AccidentTypeResponse.java
+++ src/main/java/refresh/acci/domain/analysis/adapter/in/web/dto/res/AccidentTypeResponse.java
@@ -1,15 +1,20 @@
 package refresh.acci.domain.analysis.adapter.in.web.dto.res;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import refresh.acci.domain.analysis.model.enums.AccidentType;
 
 public record AccidentTypeResponse(
 
+        @Schema(description = "사고 유형", example = "차대차")
         String objectType,
 
+        @Schema(description = "사고 장소", example = "직선 도로")
         String place,
 
+        @Schema(description = "사고 상황", example = "추돌 사고")
         String situation,
 
+        @Schema(description = "차량 A의 진행 방향", example = "선행자동차(1차사고차량)를 추돌")
         String vehicleADirection
 
 ) {
